ec63b6813deca6aa40fe0f6728b7e90312176315
shaocaoyu-01 <csha0058@student.monash.edu>
2025-11-01 19:51:13 +1100

    add the plot.R

---
+++ R/plot.R
@@ -0,0 +1,23 @@
+#' Plot heatmap by VE & R0
+#' 
+#' @param R0 different variants of corona viruses' spread capability
+#' @param VE the vaccine efficiency, the ratio it decrease the infected rate
+#' @param $\beta$_tot denotes the integrated, total force of infection arising from all simulated quarantine breach events.
+#' 
+#' @return  A plot object
+#' 
+#' Plot contour lines for beta_total
+#' 
+#' Draw contour lines over the parameter surface of `beta_total`
+#' 
+#' @param R0 different variants of corona viruses' spread capability
+#' @param VE the vaccine efficiency, the ratio it decrease the infected rate
+#' @param $\beta$_tot denotes the integrated, total force of infection arising from all simulated quarantine breach events.
+#' 
+#' Plot one-factor response of beta_total
+#' 
+#' Plot `beta_total` as a function of a single parameter while holding the other fixed.
+#' 
+#' @param R0 different variants of corona viruses' spread capability
+#' @param VE the vaccine efficiency, the ratio it decrease the infected rate
+#' @param $\beta$_tot denotes the integrated, total force of infection arising from all simulated quarantine breach events.
